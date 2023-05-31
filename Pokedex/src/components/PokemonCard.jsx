ca46d61d08c597e168b20755fd12fdd28c6fcd5f
Nestadar <Nestadar@gmail.com>
2023-05-31 14:52:51 +0200

    Bulbazaurrrrrrr

--- Pokedex/src/components/PokemonCard.jsx
+++ Pokedex/src/components/PokemonCard.jsx
@@ -1,5 +1,6 @@
+import PropTypes from "prop-types";
 function PokemonCard(props) {
-    
+    console.log(props);
     return (
     <figure>
         {props.pokemon.imgSrc != undefined ? <img src={props.pokemon.imgSrc}
@@ -9,6 +10,13 @@ function PokemonCard(props) {
     </figure>); 
   }
   
+  PokemonCard.propTypes = {
+    props: PropTypes.shape({
+     name: PropTypes.string.isRequired,
+     imgSrc: PropTypes.string,
+    }).isRequired,
+  }
+
   export default PokemonCard;
 
  
