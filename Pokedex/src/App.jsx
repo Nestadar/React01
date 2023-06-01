decf2e8e7ac37babddc2b0f7a75007c451c512b0
Nestadar <Nestadar@gmail.com>
2023-06-01 10:58:39 +0200

    Next and Previous button added

--- Pokedex/src/App.jsx
+++ Pokedex/src/App.jsx
@@ -1,27 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import MyTitle from './components/MyTitle'
 import PokemonCard from './components/PokemonCard'
 
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+
 function App() {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+  
+
+  const [pokeCount, setPokeCount] = useState(0);
+
+  const handleClickNext = () => {
+    setPokeCount(pokeCount +1)
+};  
+
+  const handleClickPrevious = () => {
+      setPokeCount(pokeCount -1)
+  };  
+
+  
 
   return (
     <div>
-     <PokemonCard pokemon = {pokemonList[0]}/>
+      <PokemonCard pokemon = {pokemonList[pokeCount]}/>
+      {pokeCount < pokemonList.length -1 ? <button onClick={handleClickNext}>Next</button> : 
+      <p></p>}
+      {pokeCount > 0 ? <button onClick={handleClickPrevious}>Previous</button> : <p></p>}
     </div>
   );
 }
 
 export default App;
+
+
